ce5d56bef974ea793e0446194e64e5b631724314
lbennett <liamjbennett@gmail.com>
2012-09-18 19:31:31 +0100

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class sabnzbd(source="true") {
+	
+	version = "0.6.10"
+	package = "SABnzbd-#{version}.tar.gz"
+	url = ""
+	
+	include sabnzbd::config
+	
+	exec { 'download-sabnzbd':
+        command => "/usr/bin/curl -L -o #{package} #{url}",
+        cwd     => '/var/tmp',
+        creates => "/var/tmp/#{package}",
+		#onlyif
+    }
+	
+	file { "/usr/local/SABnzbd-#{version}":
+		ensure => directory,
+		owner => 'root',
+		group => 'root',
+		mode => '0644',
+	}
+	
+	exec { 'unpackage-sabnzbd':
+		command => "/bin/tar xzf /var/tmp/#{package}"
+		cwd     => "/usr/local"
+		creates => "/usr/local/SABnzbd-#{version}"
+	}
+	
+	file { "/usr/local/SABnzbd":
+	    ensure => link,
+        target => "/usr/local/SABnzbd-#{version}",
+	}	
+}
