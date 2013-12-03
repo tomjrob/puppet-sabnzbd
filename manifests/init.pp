dcefb5806f0b4b55732d429e95d2a20d9cf8eba3
Tom Robinson <robinson.tj@gmail.com>
2013-12-04 00:05:32 +1000

    Change git dependency to vcsrepo to remove ugly exec command. Better to use a custom resource.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,23 @@
 class sabnzbd {
-
     include sabnzbd::params
     include sabnzbd::config
-    include git
     
     class { 'python':
       pip  => true,
       virtualenv => true,
       dev => true,
+      
       }
       
     $package_deps = ['unrar','unzip','p7zip','par2','python-yenc']
+    $pip_deps = ['pyOpenSSL','cheetah']
+    
     $venv = "${sabnzbd::params::base_dir}/sabnzbd/${sabnzbd::params::venv_dir}"
+    
     package { $package_deps:
-        ensure => 'installed'
-    }
+        ensure => 'installed';
+        
+        }
 
     user { 'sabnzbd':
         ensure    => 'present',
@@ -22,7 +25,8 @@ class sabnzbd {
         shell     => '/bin/bash',
         home      => "${sabnzbd::params::base_dir}/sabnzbd",
         password  => 'sabnzbd';
-    }
+        
+        }
 
     file { "${sabnzbd::params::base_dir}/sabnzbd":
         ensure  => directory,
@@ -30,53 +34,48 @@ class sabnzbd {
         group   => 'sabnzbd',
         mode    => '0644',
         recurse => true
-    }
+        
+        }
     
     python::virtualenv { "${venv}":
         ensure       => present,
         owner        => 'sabnzbd',
         group        => 'sabnzbd',
-        require      => File["${sabnzbd::params::base_dir}/sabnzbd"]
-		  }
+        require      => File["${sabnzbd::params::base_dir}/sabnzbd"],
+        
+        }
     
-    python::pip {'pyOpenSSL':
+    python::pip { $pip_deps:
         ensure => present,
         virtualenv => "${venv}",
         owner => 'sabnzbd',
-    }
+        
+        }
+        
+#            python::pip {'pyOpenSSL':
+#        ensure => present,
+#        virtualenv => "${venv}",
+#        owner => 'sabnzbd',
+#        
+#        }
     
-    python::pip {'cheetah':
-        ensure => present,
-        virtualenv => "${venv}",
-        owner => 'sabnzbd',
-    }
+#    python::pip {'cheetah':
+#        ensure => present,
+#        virtualenv => "${venv}",
+#        owner => 'sabnzbd',
+#        
+#        }
     
-    exec { 'download-sabnzbd':
-        command => "/usr/bin/git clone ${sabnzbd::params::url} src",
-        cwd     => "${sabnzbd::params::base_dir}/sabnzbd",
-        creates => "${sabnzbd::params::base_dir}/sabnzbd/src",
-        user    => 'sabnzbd',
-        require => Class['git'],
-    }
-    
-
-#    exec { 'install-pyopenssl':
-#        command => "${sabnzbd::params::base_dir}/sabnzbd/venv/bin/pip install pyOpenSSL",
-#        cwd     => "${sabnzbd::params::base_dir}/sabnzbd/venv",
-#        creates => "${sabnzbd::params::base_dir}/sabnzbd/venv/lib/python2.7/site-packages/OpenSSL",
-#        path    => "${sabnzbd::params::base_dir}/sabnzbd/venv/bin",
-#        user    => 'sabnzbd',
-#        require => [Python::Virtualenv["${venv}"],Class['python']];
-#    }
-#    exec { 'install-cheetah-sabnzbd':
-#        command => "${sabnzbd::params::base_dir}/sabnzbd/venv/bin/pip install cheetah",
-#        cwd     => "${sabnzbd::params::base_dir}/sabnzbd/venv",
-#        creates => "${sabnzbd::params::base_dir}/sabnzbd/venv/bin/cheetah",
-#        path    => "${sabnzbd::params::base_dir}/sabnzbd/venv/bin",
-#        user    => 'sabnzbd',
-#        require => [Python::Virtualenv["${venv}"],Class['python']];
-#    }
-    
+   vcsrepo { "${sabnzbd::params::base_dir}/sabnzbd/src":
+        ensure => latest,
+        provider => git,
+        source => "${sabnzbd::params::url}",
+        revision => master,
+        user => 'sabnzbd',
+        require => File["${sabnzbd::params::base_dir}/sabnzbd"],
+        
+        }
+  
     supervisor::service { 'sabnzbd':
         ensure         => present,
         enable         => true,
@@ -86,6 +85,8 @@ class sabnzbd {
         user           => 'sabnzbd',
         group          => 'sabnzbd',
         directory      => "${sabnzbd::params::base_dir}/sabnzbd/src/",
-        require        => [Exec['download-sabnzbd'],Class['python']];
-    }
-}
+        require        => [Vcsrepo["${sabnzbd::params::base_dir}/sabnzbd/src"],Python::Virtualenv["${venv}"]];
+        
+        }
+        
+        }
