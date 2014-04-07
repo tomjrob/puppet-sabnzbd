abdd1b6fcd6bb510268bfcd6365b66b27278b33e
Tom Robinson <robinson.tj@gmail.com>
2014-04-08 05:18:29 +1000

    Bare Bones Installer Working with hard coded settings

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,5 @@
 class sabnzbd::config inherits sabnzbd {
-  file { "${config_dir}/sickbeard.ini":
+  file { "${config_dir}/sabnzbd.ini":
     content => template('sabnzbd/sabnzbd.ini.erb'),
     owner  => "${user}",
     group  => "${group}",
